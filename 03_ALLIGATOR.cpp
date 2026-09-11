1a241cf7af88d3d8a88ffa69d8240a7ca2c22047
Tanaya Dinesh <tanayadhondre@gmail.com>
2026-09-11 23:28:52 +0530

    Add files via upload

---
+++ 03_ALLIGATOR.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+std::cout<<"See ya later, alligator!\n";
+std::cout<<"Farewell\n";
+std::cout<<"Take care\n";
+std::cout<<"Catch you later\n";
+std::cout<<"Goodnight\n";
+std::cout<<"Bye,Felicia\n";
+std::cout<<"I'm out\n";
+}
---
+++ 05_SIGN_BUNNY.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"|---------------|\n";
+  cout<<"|   MESSAGE      |\n";
+  cout<<"|    HERE         |\n";
+  cout<<"|-----------------|\n";
+  cout<<"(\__/) ||\n";
+  cout<<"(•ㅅ•) ||\n";
+  cout<<"/ 　 づ";
+}
+
