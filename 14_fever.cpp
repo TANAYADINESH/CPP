b19387e2c5e333c0ad1b59cc62c9b66f7fa30b37
Tanaya Dinesh <tanayadhondre@gmail.com>
2026-09-13 15:53:41 +0530

    Add files via upload

---
+++ 14_fever.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ double temp = 98.0;
+ if (temp >=100){
+   cout<<"You have a fever.🤒";
+ }
+ else {
+   cout<<"You do not have a fever.🕺";
+ }
+}
+  
+
